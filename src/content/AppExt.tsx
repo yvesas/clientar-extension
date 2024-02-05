9a4a1cde4de35c3c3d083dc6e1811a013ebd0f93
Yves <yves.as@gmail.com>
2024-02-05 14:18:37 -0300

    catchers

--- src/content/AppExt.tsx
+++ src/content/AppExt.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, {useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { Container } from "../components/Container";
 import { Button } from "../components/Button";
@@ -9,59 +9,69 @@ import { generateID } from "../shared/generateID";
 import { IMessageObject } from "../shared/IMessageObject";
 import { sortMessages } from "../shared/sortMessages";
 
-
 export function AppExt(): React.ReactElement {
   const [isFirstRender, setIsFirstRender] = useState(true);
   const [copiedText, setCopiedText] = useState<string | null>(null);
-  const [selectMsgs, setSelectMsgs] = useState(false)
+  const [selectMsgs, setSelectMsgs] = useState(false);
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
   const [messages, setMessages] = useState<IMessageObject[]>([]);
-  
+
   useEffect(() => {
-    if(selectMsgs){
-      setErrorMsg(null)
-    }       
-  }, [selectMsgs, isFirstRender])
+    if (selectMsgs) {
+      setErrorMsg(null);
+    }
+  }, [selectMsgs, isFirstRender]);
 
   const addEventClearAll = () => {
-    if(isFirstRender){
-      const menuWpElements = document.querySelectorAll("#app ._3RGKj .g0rxnol2"); 
-      if(menuWpElements && menuWpElements.length>0){        
-        menuWpElements.forEach((ele) => { 
-          ele.addEventListener("click", () => {
-            clearDataAction()
+    try {
+      if (isFirstRender) {
+        const menuWpElements = document.querySelectorAll(
+          "#app ._3RGKj .g0rxnol2"
+        );
+        if (menuWpElements && menuWpElements.length > 0) {
+          menuWpElements.forEach((ele) => {
+            ele.addEventListener("click", () => {
+              clearDataAction();
+            });
           });
-         })
-        setIsFirstRender(false)
-      }
-      // chrome.runtime.onMessage.addListener(function(message, sender) {
-      //   console.log('>>> CONTENT WPP recebeu! >>> sender:', sender)
-      //   if (message.action === "sendTextEXT") {
-          
-      //     const data = message.message
-      //     console.log('>>> CONTENT WPP recebeu! --> ', data)
-          
-      //   }
-      // });
-    }    
-  }
-  addEventClearAll()
+          setIsFirstRender(false);
+        }
+        // chrome.runtime.onMessage.addListener(function(message, sender) {
+        //   console.log('>>> CONTENT WPP recebeu! >>> sender:', sender)
+        //   if (message.action === "sendTextEXT") {
 
-  const addMessage = (messageObject: IMessageObject) => { 
-    if(messageObject.id && messageObject.title && messageObject.message){
-      setMessages(old => [...old, messageObject]);
-    }           
+        //     const data = message.message
+        //     console.log('>>> CONTENT WPP recebeu! --> ', data)
+
+        //   }
+        // });
+      }
+    } catch (err) {
+      console.error("Failed add event Clear. ", err);
+    }
+  };
+  addEventClearAll();
+
+  const addMessage = (messageObject: IMessageObject) => {
+    if (messageObject.id && messageObject.title && messageObject.message) {
+      setMessages((old) => [...old, messageObject]);
+    }
   };
   const removeMessage = (id: string) => {
-    setMessages((prevMessages) => prevMessages.filter((message) => message.id !== id));
+    setMessages((prevMessages) =>
+      prevMessages.filter((message) => message.id !== id)
+    );
   };
-  
-  const replaceEmoticon = async (text: string | null, altValue: string | undefined): Promise<string | null> => {                
-    if(!altValue){
-      return text
+
+  const replaceEmoticon = async (
+    text: string | null,
+    altValue: string | undefined
+  ): Promise<string | null> => {
+    if (!altValue) {
+      return text;
     }
-    if(!text){
-      return text
+    if (!text) {
+      return text;
     }
     return new Promise((resolve, reject) => {
       try {
@@ -73,26 +83,30 @@ export function AppExt(): React.ReactElement {
             } else {
               return match;
             }
-          },
+          }
         );
-        resolve(newText); 
-    } catch (error) {
-      reject(error); 
-    }
+        resolve(newText);
+      } catch (error) {
+        reject(error);
+      }
     });
-  }
+  };
 
-  const replaceLinkSource = async (text: string | null, hrefValue: string | undefined): Promise<string | null> => {    
+  const replaceLinkSource = async (
+    text: string | null,
+    hrefValue: string | undefined
+  ): Promise<string | null> => {
     if (!hrefValue) {
       return text;
     }
     if (!text) {
       return text;
-    }  
+    }
     return new Promise((resolve, reject) => {
       try {
-        const newText = text.replace(/<a[^>]*?\s*href="(.+?)"[^>]*?>\s*\n?\s*(?:.*?)<\/a>/i,
-            (match, href) => {
+        const newText = text.replace(
+          /<a[^>]*?\s*href="(.+?)"[^>]*?>\s*\n?\s*(?:.*?)<\/a>/i,
+          (match, href) => {
             if (href.trim() === hrefValue.trim()) {
               return ` ${hrefValue.trim()} `;
             } else {
@@ -106,164 +120,215 @@ export function AppExt(): React.ReactElement {
       }
     });
   };
-  
-  const copyAction = async () => {  
-      if(messages && messages.length > 0){
-        const messagesOrdered = await sortMessages(messages)      
-        let fullText =  ''
-        messagesOrdered.forEach((item:any, index:any) => { 
-          if(index>0){
-            fullText +=  item.title+' '+item.message + '\n'
-          }else{
-            fullText =  item.title+' '+item.message + '\n'
-          }          
-        });        
+
+  const copyAction = async () => {
+    try {
+      if (messages && messages.length > 0) {
+        const messagesOrdered = await sortMessages(messages);
+        let fullText = "";
+        messagesOrdered.forEach((item: any, index: any) => {
+          if (index > 0) {
+            fullText += item.title + " " + item.message + "\n";
+          } else {
+            fullText = item.title + " " + item.message + "\n";
+          }
+        });
         navigator.clipboard.writeText(fullText);
-        const clipText = await navigator.clipboard.readText()
-        if(clipText){
-          setCopiedText(clipText)        
-          clearDataAction()
+        const clipText = await navigator.clipboard.readText();
+        if (clipText) {
+          setCopiedText(clipText);
+          clearDataAction();
           // console.log('>> GO Send Message >>')
           // chrome.runtime.sendMessage({
           //   action: "sendTextEXT",
           //   message: clipText
           // })
-          
-        }        
-      }       
-  }
-
-  const getText = async (id: string, checked: boolean) => { 
-    if(!checked){
-      removeMessage(id)
-      
-    }else{  
-      const msgObj:IMessageObject = {
-        id: id,
-        title: null,
-        message: null
-      }
-      const selectorCopyableText = '[extapp="'+"ext-"+ id + '"] .copyable-text';
-      const copyableTextNodes = document.querySelectorAll(selectorCopyableText)          
-
-      copyableTextNodes.forEach(async (node) => {  
-        if(node.nodeName == 'DIV'){
-          msgObj.title = node.attributes.getNamedItem('data-pre-plain-text')?.value;        
         }
-        if(node.localName == 'span'){
-          const elementText = node.firstChild as HTMLElement
-          msgObj.message = elementText.innerHTML
+      }
+    } catch (err) {
+      console.error("Failed copy action. ", err);
+    }
+  };
 
-          if(elementText.localName == 'span'){
-            for (let index = 0; index < elementText.children.length; index++) {
-              const child = elementText.children[index] as HTMLElement
-              if(child.tagName=="IMG" ){
-                const altAttribute = child.attributes.getNamedItem('alt')?.value;            
-                msgObj.message = await replaceEmoticon(msgObj.message, altAttribute)                                    
-              }
-              if(child.tagName=="A" ){
-                const hrefAttribute = child.attributes.getNamedItem('href')?.value;                          
-                msgObj.message = await replaceLinkSource(msgObj.message, hrefAttribute)  
+  const getText = async (id: string, checked: boolean) => {
+    try {
+      if (!checked) {
+        removeMessage(id);
+      } else {
+        const msgObj: IMessageObject = {
+          id: id,
+          title: null,
+          message: null,
+        };
+        const selectorCopyableText =
+          '[extapp="' + "ext-" + id + '"] .copyable-text';
+        const copyableTextNodes =
+          document.querySelectorAll(selectorCopyableText);
+
+        copyableTextNodes.forEach(async (node) => {
+          if (node.nodeName == "DIV") {
+            msgObj.title = node.attributes.getNamedItem(
+              "data-pre-plain-text"
+            )?.value;
+          }
+          if (node.localName == "span") {
+            const elementText = node.firstChild as HTMLElement;
+            msgObj.message = elementText.innerHTML;
+
+            if (elementText.localName == "span") {
+              for (
+                let index = 0;
+                index < elementText.children.length;
+                index++
+              ) {
+                const child = elementText.children[index] as HTMLElement;
+                if (child.tagName == "IMG") {
+                  const altAttribute =
+                    child.attributes.getNamedItem("alt")?.value;
+                  msgObj.message = await replaceEmoticon(
+                    msgObj.message,
+                    altAttribute
+                  );
+                }
+                if (child.tagName == "A") {
+                  const hrefAttribute =
+                    child.attributes.getNamedItem("href")?.value;
+                  msgObj.message = await replaceLinkSource(
+                    msgObj.message,
+                    hrefAttribute
+                  );
+                }
               }
             }
           }
-          
-          
-          
-          
-        }
-      })
-      addMessage(msgObj) 
-    }   
-  }
-
-  const createContainerCheckBox = (parent:HTMLElement, uniqueID:string) => {
-    const root = document.createElement("div");
-    root.id = "crx-root-chkbx";
-    parent.appendChild(root);
-
-    parent.style.setProperty("display", "flex", 'important') 
-    parent.style.setProperty("align-items", "center", 'important')       
-    parent.style.setProperty("justify-content", "flex-start", 'important')       
-
-    ReactDOM.createRoot(root).render(
-      <React.StrictMode>
-        <CheckboxStyle id={uniqueID} checked={false} onChange={getText}/>
-      </React.StrictMode>
-    );
-  }
-
-  const showSelectMessages = async() => {
-    const rowsChats = document.querySelectorAll('#main [role="application"] [role="row"]')    
-    if(rowsChats && rowsChats.length>0){
-      clearOutput()
-      setSelectMsgs(true)
-      rowsChats.forEach((row) => {      
-        row.querySelector('#crx-root-chkbx')?.remove() //remove duplicate items.
-        
-        const uniqueID = generateID();
-        row.setAttribute("extapp", "ext-"+uniqueID)
-
-        createContainerCheckBox(row as HTMLElement, uniqueID)
-      });    
-    }{
-      setErrorMsg('Entre em uma conversa para selecionar as mensagens.')
+        });
+        addMessage(msgObj);
+      }
+    } catch (err) {
+      console.error("Failed get text. ", err);
     }
-  }
+  };
+
+  const createContainerCheckBox = (parent: HTMLElement, uniqueID: string) => {
+    try {
+      const root = document.createElement("div");
+      root.id = "crx-root-chkbx";
+      parent.appendChild(root);
+
+      parent.style.setProperty("display", "flex", "important");
+      parent.style.setProperty("align-items", "center", "important");
+      parent.style.setProperty("justify-content", "flex-start", "important");
+
+      ReactDOM.createRoot(root).render(
+        <React.StrictMode>
+          <CheckboxStyle id={uniqueID} checked={false} onChange={getText} />
+        </React.StrictMode>
+      );
+    } catch (err) {
+      console.error("Failed create checkbox container. ", err);
+    }
+  };
+
+  const showSelectMessages = async () => {
+    try {
+      const rowsChats = document.querySelectorAll(
+        '#main [role="application"] [role="row"]'
+      );
+      if (rowsChats && rowsChats.length > 0) {
+        clearOutput();
+        setSelectMsgs(true);
+        rowsChats.forEach((row) => {
+          row.querySelector("#crx-root-chkbx")?.remove(); //remove duplicate items.
+
+          const uniqueID = generateID();
+          row.setAttribute("extapp", "ext-" + uniqueID);
+
+          createContainerCheckBox(row as HTMLElement, uniqueID);
+        });
+      }
+      {
+        setErrorMsg("Entre em uma conversa para selecionar as mensagens.");
+      }
+    } catch (err) {
+      console.error("Failed show checkbox. ", err);
+    }
+  };
 
   const removeSelectMessages = () => {
-    const rowsChats = document.querySelectorAll('#main [role="application"] [role="row"]')    
-    if(rowsChats){
-      setSelectMsgs(false)
-      rowsChats.forEach((row) => {      
-        row.querySelector('#crx-root-chkbx')?.remove()              
-      });
-
+    try {
+      const rowsChats = document.querySelectorAll(
+        '#main [role="application"] [role="row"]'
+      );
+      if (rowsChats) {
+        setSelectMsgs(false);
+        rowsChats.forEach((row) => {
+          row.querySelector("#crx-root-chkbx")?.remove();
+        });
+      }
+    } catch (err) {
+      console.error("Failed remove checkbox. ", err);
     }
-  }
+  };
 
   const selectAllMessages = () => {
-    const checkboxes = document.querySelectorAll('#main [data-extapp="chckbx"]');
-    if(checkboxes){
-      checkboxes.forEach((box) => {
-        (box as HTMLElement).click();
-      })
+    try {
+      const checkboxes = document.querySelectorAll(
+        '#main [data-extapp="chckbx"]'
+      );
+      if (checkboxes) {
+        checkboxes.forEach((box) => {
+          (box as HTMLElement).click();
+        });
+      }
+    } catch (err) {
+      console.error("Failed select all messages. ", err);
     }
-
-  }
+  };
 
   const clearDataAction = () => {
-    setMessages([])
+    setMessages([]);
     removeSelectMessages();
-    clearOutput()
-  }
+    clearOutput();
+  };
 
   const clearOutput = () => {
-    const output = document.querySelector('#output') ? document.querySelector('#output'):null
-    if(output)
-    output.innerHTML = "";
-  }
+    const output = document.querySelector("#output")
+      ? document.querySelector("#output")
+      : null;
+    if (output) output.innerHTML = "";
+  };
 
   return (
     <>
       <Container>
-      {selectMsgs ? 
-      (<div className="flex flex-col gap-y-4">
-        <Button id="selectAll" onClick={selectAllMessages} >Selecionar mensagens recentes</Button>
-        
-        {messages && messages.length>0 &&
-        (<Button id="copyButton" onClick={copyAction} >Copiar as mensagens</Button>)
-        }
-        
-      </div>) 
-      :
-      (<Button id="selectMsgBtn" onClick={showSelectMessages} >Selecionar mensagens</Button>)
-      }
-      {errorMsg && (<span className="text-sm text-pretty text-red-500 tracking-wide">{errorMsg}</span>) }
+        {selectMsgs ? (
+          <div className="flex flex-col gap-y-4">
+            <Button id="selectAll" onClick={selectAllMessages}>
+              Selecionar mensagens recentes
+            </Button>
 
-      <ShowText id="output" >{copiedText}</ShowText>
-      <Button id="cancel" typeButton="danger" onClick={clearDataAction} >Apagar mensagens</Button>       
+            {messages && messages.length > 0 && (
+              <Button id="copyButton" onClick={copyAction}>
+                Copiar as mensagens
+              </Button>
+            )}
+          </div>
+        ) : (
+          <Button id="selectMsgBtn" onClick={showSelectMessages}>
+            Selecionar mensagens
+          </Button>
+        )}
+        {errorMsg && (
+          <span className="text-sm text-pretty text-red-500 tracking-wide">
+            {errorMsg}
+          </span>
+        )}
+
+        <ShowText id="output">{copiedText}</ShowText>
+        <Button id="cancel" typeButton="danger" onClick={clearDataAction}>
+          Apagar mensagens
+        </Button>
       </Container>
     </>
-  )
+  );
 }
--- src/content/AppExtCRM.tsx
+++ src/content/AppExtCRM.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useState} from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { ButtonClip } from "../components/ButtonClip";
 
@@ -10,89 +10,134 @@ export function AppExtCRM(): React.ReactElement {
   //     console.log('>> CALL useEffect')
   // }, [isFirstRender])
 
-  const validateClipTextIsWpp = (text:string) => {
+  const validateClipTextIsWpp = (text: string) => {
+    try {
       const regex = /\[(\d+):(\d+), (\d+)\/(\d+)\/(\d+)\]/g;
       const matches = text.match(regex);
-      if(matches && matches.length>0){
-        return true
-      }else{
-        return false
+      if (matches && matches.length > 0) {
+        return true;
+      } else {
+        return false;
       }
-  }
-  
-  const removeClipButtons = async () => {
-    navigator.clipboard.writeText('a');
-    const clearClipText = await navigator.clipboard.readText()        
-    const elements = document.querySelectorAll('#crx-root-btn')  
-    if(elements && clearClipText){
-      elements.forEach((ele) => {      
-        ele?.remove()              
-      });    
-    }
-  }
-
-  const clipMessage = async () => {
-    const clipText = await navigator.clipboard.readText();
-    if (clipText && validateClipTextIsWpp(clipText)) {
-      
-      const textArea = document.querySelector("#quill-container div.ql-editor p") as HTMLElement;    
-      textArea.innerHTML = clipText
-
-      removeClipButtons()
+    } catch (err) {
+      console.error("Failed validate text. ", err);
     }
   };
- 
-  const createContainerButtonClip = () => {
-    const commentButtonContainer = document.querySelector("div.editor__button") as HTMLElement;    
 
-    if(commentButtonContainer){ 
-      commentButtonContainer.querySelector('#crx-root-btn')?.remove()   
+  const removeClipButtons = async () => {
+    try {
+      navigator.clipboard.writeText("a");
+      // const clearClipText = await navigator.clipboard.readText()
+      const elements = document.querySelectorAll("#crx-root-btn");
+      if (elements) {
+        elements.forEach((ele) => {
+          ele?.remove();
+        });
+      }
+    } catch (err) {
+      console.error("Failed remove clip button. ", err);
+    }
+  };
+
+  const clipMessage = async () => {
+    try {
+      const clipText = await navigator.clipboard.readText();
+      if (clipText && validateClipTextIsWpp(clipText)) {
+        const textArea = document.querySelector(
+          "#quill-container div.ql-editor p"
+        ) as HTMLElement;
+        textArea.innerHTML = clipText;
+
+        removeClipButtons();
+      }
+    } catch (err) {
+      console.error("Failed clip message. ", err);
+      return;
+    }
+  };
+
+  const createContainerButtonClip = () => {
+    try {
+      const commentButtonContainer = document.querySelector(
+        "div.editor__button"
+      ) as HTMLElement;
+
+      if (commentButtonContainer) {
+        commentButtonContainer.querySelector("#crx-root-btn")?.remove();
         // const uniqueID = generateID();
         // commentButtonContainer.setAttribute("ext-container", "ext-"+uniqueID)
         const root = document.createElement("div");
         root.id = "crx-root-btn";
         commentButtonContainer.appendChild(root);
 
-        commentButtonContainer.style.setProperty("display", "flex", 'important') 
-        commentButtonContainer.style.setProperty("align-items", "center", 'important')       
-        commentButtonContainer.style.setProperty("justify-content", "flex-start", 'important')
-        commentButtonContainer.style.setProperty("gap", "15px 15px", "important");
-        
+        commentButtonContainer.style.setProperty(
+          "display",
+          "flex",
+          "important"
+        );
+        commentButtonContainer.style.setProperty(
+          "align-items",
+          "center",
+          "important"
+        );
+        commentButtonContainer.style.setProperty(
+          "justify-content",
+          "flex-start",
+          "important"
+        );
+        commentButtonContainer.style.setProperty(
+          "gap",
+          "15px 15px",
+          "important"
+        );
+
         ReactDOM.createRoot(root).render(
           <React.StrictMode>
-            <ButtonClip id="clipAction" typeButton="new" onClick={clipMessage} >Colar</ButtonClip>       
+            <ButtonClip id="clipAction" typeButton="new" onClick={clipMessage}>
+              Colar
+            </ButtonClip>
           </React.StrictMode>
         );
       }
+    } catch (err) {
+      console.error("Failed create clip button container. ", err);
+      return;
     }
-    
-    // const haveNewMessages = async () => {
-    //   try{
-    //     const clipText = await navigator.clipboard.readText();
-    //     if (clipText && validateClipTextIsWpp(clipText)) {      
-    //       return true
-    //     }else{
-    //       return false
-    //     }
-    //   }catch(err){
-    //     return false
-    //   }      
-    // }        
+  };
 
-    const showButtonClip =  async () => {
-      const commentButtonContainer = document.querySelector("div.editor__button") as HTMLElement;
-      if(commentButtonContainer){
-        const clipButton = commentButtonContainer.querySelector('#crx-root-btn')
-        if(clipButton){
-          return
-        // }else if(await haveNewMessages()){
-        }else {
-          createContainerButtonClip()
-        }        
+  // const haveNewMessages = async () => {
+  //   try{
+  //     const clipText = await navigator.clipboard.readText();
+  //     if (clipText && validateClipTextIsWpp(clipText)) {
+  //       return true
+  //     }else{
+  //       return false
+  //     }
+  //   }catch(err){
+  //     return false
+  //   }
+  // }
+
+  const showButtonClip = async () => {
+    try {
+      const commentButtonContainer = document.querySelector(
+        "div.editor__button"
+      ) as HTMLElement;
+      if (commentButtonContainer) {
+        const clipButton =
+          commentButtonContainer.querySelector("#crx-root-btn");
+        if (clipButton) {
+          return;
+          // }else if(await haveNewMessages()){
+        } else {
+          createContainerButtonClip();
+        }
       }
+    } catch (err) {
+      console.error("Failed show clip button. ", err);
+      return;
     }
-  
-  
+  };
 
   // const wantingMessages = () => {
   //   console.log('GO CREATE event mousemove')
@@ -100,17 +145,17 @@ export function AppExtCRM(): React.ReactElement {
   //     console.log('CALL event mousemove')
   //     verifyNewMessages()
   //   });
-    // chrome.tabs.onActivated.addListener
-    // chrome.runtime.onMessage.addListener()
+  // chrome.tabs.onActivated.addListener
+  // chrome.runtime.onMessage.addListener()
   // }
   // wantingMessages()
-  
-  const observer = new MutationObserver(() => {        
+
+  const observer = new MutationObserver(() => {
     // for (const mutation of mutations) {
     //   if (mutation.type === 'childList') {
     //     for (const addedNode of mutation.addedNodes) {
     //       if (addedNode.classList.contains('editor__button')) {
-                showButtonClip()
+    showButtonClip();
     //       }
     //     }
     //   }
@@ -118,25 +163,29 @@ export function AppExtCRM(): React.ReactElement {
   });
 
   const addListeners = () => {
-    if(isFirstRender){
-      setIsFirstRender(false)
-      // chrome.runtime.onMessage.addListener(function(message, sender) {
-      //   console.log('>>> CONTENT CRM recebeu! >>> sender:', sender)
-      //   if (message.action === "sendTextEXT") {
-          
-      //     const data = message.message
-      //     console.log('>>> CONTENT CRM recebeu! --> ', data)
-          
-      //   }
-      // });
+    try {
+      if (isFirstRender) {
+        setIsFirstRender(false);
+        // chrome.runtime.onMessage.addListener(function(message, sender) {
+        //   console.log('>>> CONTENT CRM recebeu! >>> sender:', sender)
+        //   if (message.action === "sendTextEXT") {
 
-      observer.observe(document.body, {
-        childList: true,
-        subtree: true,
-      });
+        //     const data = message.message
+        //     console.log('>>> CONTENT CRM recebeu! --> ', data)
+
+        //   }
+        // });
+
+        observer.observe(document.body, {
+          childList: true,
+          subtree: true,
+        });
+      }
+    } catch (err) {
+      console.error("Failed add listeners. ", err);
     }
-  }
-  addListeners()
+  };
+  addListeners();
 
   return (
     <>
@@ -148,5 +197,5 @@ export function AppExtCRM(): React.ReactElement {
       <Button id="cancel" typeButton="danger" onClick={cancelAction} >Apagar mensagens</Button>       
       </Container> */}
     </>
-  )
+  );
 }
