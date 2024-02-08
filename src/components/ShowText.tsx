20763e62d24757d06497ff9806fb0310f6ece6b0
Yves <yves.as@gmail.com>
2024-02-08 17:12:48 -0300

    output size

--- src/components/ShowText.tsx
+++ src/components/ShowText.tsx
@@ -9,7 +9,7 @@ export function ShowText({ id, children }: ShowTextProps) {
   return (
     <div className='flex flex-col gap-y-2'>
     <span className='text-gray-800'>Mensagens copiadas:</span>
-    <div id={id} className='w-[full] min-h-20 overflow-auto max-h-28 p-2 border border-slate-400 rounded-md text-sm text-pretty text-slate-700 tracking-normal'>
+    <div id={id} className='w-[full] min-h-20 overflow-auto max-h-72 p-2 border border-slate-400 rounded-md text-sm text-pretty text-slate-700 tracking-normal'>
       {children}
     </div>
     </div>
